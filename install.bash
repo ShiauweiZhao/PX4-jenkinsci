30516652f610d35dd85bdd34ea3f7dd77d5f981a
zhaoxiaowei <zhaoxiaowei@efy-tech.com>
2022-04-30 05:03:27 -0700

    update not test

--- install.bash
+++ install.bash
@@ -17,19 +17,22 @@ sudo mkdir -p /build/jenkins
 sudo chmod 700 /build/docker
 
 # Install Java, only headless runtime
-sudo apt-get install default-jre-headless
+sudo apt install openjdk-11-jre
 
 # Install Tomcat
 # > not necessary, using jenkins standalone
 #sudo apt-get install tomcat7
 
 # Install Apache (as proxy)
-sudo apt-get install apache2
+# sudo apt-get install apache2
 
 # Install Jenkins
-# > https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list.d/jenkins-ci.list'
+# > https://www.jenkins.io/doc/book/installing/linux/#debianubuntu
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins
 
@@ -42,19 +45,33 @@ sudo service jenkins start
 sudo rm -r /var/lib/jenkins
 
 # Configure proxy
-sudo a2enmod proxy
-sudo a2enmod proxy_http
-sudo a2dissite 000-default
-sudo cp jenkins.site /etc/apache2/sites-available/jenkins.conf
-sudo a2ensite jenkins
-sudo apache2ctl restart
+# sudo a2enmod proxy
+# sudo a2enmod proxy_http
+# sudo a2dissite 000-default
+# sudo cp jenkins.site /etc/apache2/sites-available/jenkins.conf
+# sudo a2ensite jenkins
+# sudo apache2ctl restart
 
 # Install Docker
 # > http://docs.docker.com/installation/ubuntulinux/#docker-maintained-package-installation
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install lxc-docker
+sudo apt-get install \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+# sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+# sudo apt-get update
+# sudo apt-get install lxc-docker
 
 sudo sh -c 'echo DOCKER_OPTS=\"\${DOCKER_OPTS} -H unix:///var/run/docker.sock -g /build/docker\" >> /etc/default/docker'
 sudo service docker restart
